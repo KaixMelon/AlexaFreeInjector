030bc0e8b374d6dee48acf5443ab93dd60d76213
KaixMelon <macarubbokhervin74@gmail.com>
2025-06-16 23:56:07 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/video1')
 def send_video():
-    return send_from_directory('.', 'lv_1_1234567.mp4', mimetype='video1/mp4')
+    return send_from_directory('.', 'lv_1_1234567.mp4', mimetype='video/mp4')
 
 def keep_alive():
     Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
