10ffcecf921223454220830125c3ed76f833c2a4
KaixMelon <macarubbokhervin74@gmail.com>
2025-06-16 23:38:35 +0800

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Alexa Free Bot is Alive."
+
+@app.route('/video')
+def send_video():
+    return send_from_directory('.', 'lv_1_1234567.mp4', mimetype='video/mp4')
+
+def keep_alive():
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
