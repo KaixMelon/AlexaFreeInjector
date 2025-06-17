0d107c52d9f7688508ff61a76db180a6e8831408
KaixMelon <macarubbokhervin74@gmail.com>
2025-06-18 01:53:12 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -11,5 +11,9 @@ def home():
 def send_video():
     return send_from_directory('.', 'lv_1_1234567.mp4', mimetype='video/mp4')
 
+@app.route('/ping')
+def ping():
+    return "Pong"
+
 def keep_alive():
     Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
