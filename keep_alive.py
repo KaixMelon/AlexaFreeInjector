3ae4c146375c7e8d8d66823b43a6e3762d8901c6
KaixMelon <macarubbokhervin74@gmail.com>
2025-07-15 09:53:43 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def home():
     return "✅ Alexa Free Bot is Alive."
 
-@app.route('/video2025')
+@app.route('/videoJuly15')
 def send_video():
-    return send_from_directory('.', 'lv_0_20250708133857.mp4', mimetype='video/mp4')
+    return send_from_directory('.', 'lv_0_20250715094318.mp4', mimetype='video/mp4')
 
 def keep_alive():
     Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
