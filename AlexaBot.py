4bd43d1d66ba6852389b0833d8728bf108bd6684
KaixMelon <macarubbokhervin74@gmail.com>
2025-07-24 03:59:46 +0800

    Update AlexaBot.py

--- AlexaBot.py
+++ AlexaBot.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Set this in your Render environment
 
 # Inline buttons
 BUTTONS = InlineKeyboardMarkup([
-    [InlineKeyboardButton("📝 Register", url="https://lootdest.org/s?J5ZVkCx4&data=XD/SwHNd620VS9HV9NwpOvqh2LWg1ONIiAuQPE2R4mo%3D")],
+    [InlineKeyboardButton("📝 Register", url="https://kaicodm.store/Free/register.php")],
     [InlineKeyboardButton("📽 Tutorial", url="https://t.me/AlexaMainChannel2/693")]
 ])
 
